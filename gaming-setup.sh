20383f28a98b67272fcb7729aa3380da5cab9f59
Tony-855 <antonyflores3030@gmail.com>
2026-02-05 20:13:11 -0300

    gaming-setup.sh

--- gaming-setup.sh
+++ gaming-setup.sh
@@ -63,7 +63,7 @@ case $gpu in
     1)
         echo "Instalando drivers Nvidia..."
         sudo pacman -S --needed --noconfirm linux-headers base-devel dkms vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa lib32-opencl-nvidia vulkan-tools mesa-utils nvtop
-        yay -S nvidia-580xx-dkms nvidia-580xx-utils lib32-nvidia-580xx-utils nvidia-580xx-settings
+        yay -S --needed --noconfirm nvidia-580xx-dkms nvidia-580xx-utils lib32-nvidia-580xx-utils nvidia-580xx-settings
         sudo systemctl enable --now nvidia-persistenced.service
         sudo mkinitcpio -P
         ;;
