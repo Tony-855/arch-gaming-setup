5eb5bd19d9a57c707711d67008ca98206e9579e5
Tony-855 <antonyflores3030@gmail.com>
2026-03-05 04:00:51 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -84,6 +84,7 @@ fi
  else
          log_info "Instalando Chaotic"
          install_chaotic
+         sudo pacman -Syy --noconfirm --needed
          if check_chaotic; then
                  log_ok "Chaotic instalado correctamente"
          else
